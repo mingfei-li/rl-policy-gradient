e4452649dce3a5c2bf3acda162a3009d0d403f45
Mingfei Li <ml@mingfei.io>
2024-06-12 10:29:28 -0700

    Add tensorboard logging
    already achieving 200 reward

--- train.py
+++ train.py
@@ -1,13 +1,17 @@
+from collections import deque
 from gymnasium.experimental.wrappers import RecordVideoV0
+from statistics import mean
 from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
 import gymnasium as gym
 import torch
 import torch.nn as nn
 
 class Config():
     n_training_episodes = 5000
-    lr = 0.01
+    lr = 0.001
     gamma = 1
+    exp_id = 'exp-3'
 
 class MLP(nn.Module):
     def __init__(self, in_features, out_features):
@@ -24,10 +28,27 @@ class MLP(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+class Logger():
+    def __init__(self, log_dir):
+        self.scalar_buffer = {}
+        self.writer = SummaryWriter(log_dir)
+
+    def add_scalar(self, key, value):
+        if key not in self.scalar_buffer.keys():
+            self.scalar_buffer[key] = deque(maxlen=50)
+        self.scalar_buffer[key].append(value)
+
+    def flush(self, t):
+        for key, value in self.scalar_buffer.items():
+            self.writer.add_scalar(key, mean(value), t)
+        self.writer.flush()
+
 def train():
-    env = gym.make('CartPole-v0', render_mode='rgb_array')
-    env = RecordVideoV0(env, video_folder='results/videos')
     config = Config()
+
+    env = gym.make('CartPole-v0', render_mode='rgb_array')
+    env = RecordVideoV0(env, video_folder=f'results/{config.exp_id}/videos')
+    logger = Logger(log_dir=f'results/{config.exp_id}/logs')
 
     model = MLP(
         in_features=env.observation_space.shape[0],
@@ -36,7 +57,6 @@ def train():
     optimizer = torch.optim.Adam(model.parameters(), lr=config.lr)
 
     for i in tqdm(range(config.n_training_episodes)):
-        print(f'In episode {i}')
         states = []
         actions = []
         rewards = []
@@ -46,9 +66,7 @@ def train():
             model.eval()
             with torch.no_grad():
                 pi = model(torch.tensor(state).unsqueeze(dim=0))[0]
-            print(f'pi = {pi}')
             action = torch.multinomial(pi, num_samples=1).item()
-            print(f'action = {action}')
             states.append(state)
             actions.append(action)
             state, reward, terminated, truncated, _ = env.step(action)
@@ -69,6 +87,12 @@ def train():
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        logger.add_scalar('episode_len', len(g))
+        logger.add_scalar('reward', g[0].item())
+        logger.add_scalar('loss', loss.item())
+        logger.add_scalar('lr', config.lr)
+        logger.flush(i)
 
     env.close()
 
