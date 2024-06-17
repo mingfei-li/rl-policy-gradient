ed94943dec45747d0fa48f9d793566fc74a44679
Mingfei Li <ml@mingfei.io>
2024-06-17 13:31:32 -0700

    exp-13

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 class Config():
     n_training_episodes = 5000
-    policy_network_lr = 1e-5
+    policy_network_lr = 1e-8
     baseline_network_lr = 0.001
     gamma = 1
     use_baseline = True
     record_freq = 1000
     discrete = False
     game = 'InvertedPendulum-v4'
-    exp_id = 'exp-12'
+    exp_id = 'exp-13'
