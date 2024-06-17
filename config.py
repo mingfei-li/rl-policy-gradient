856e0d94ec51dd05f6ede7d3ed159ea0810a2208
Mingfei Li <ml@mingfei.io>
2024-06-17 12:50:59 -0700

    exp-7

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config():
     n_training_episodes = 5000
-    lr = 0.01
+    lr = 0.1
     gamma = 1
     use_baseline = True
     record_freq = 1000
     discrete = False
     game = 'InvertedPendulum-v4'
-    exp_id = 'exp-6'
+    exp_id = 'exp-7'
