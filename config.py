b16af7a4ad8368ad0ba896fe9456773a5fd7116a
Mingfei Li <ml@mingfei.io>
2024-06-20 14:56:07 -0700

    exp-27

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config():
     n_batches = 5000
     batch_size = 100
-    policy_network_lr = 5e-3
+    policy_network_lr = 1e-3
     baseline_network_lr = 1e-3
     gamma = 1
     n_batches_baseline = 2
@@ -10,4 +10,4 @@ class Config():
     # game = 'CartPole-v0'
     discrete = False
     game = 'InvertedPendulum-v4'
-    exp_id = 'exp-26'
+    exp_id = 'exp-27'
