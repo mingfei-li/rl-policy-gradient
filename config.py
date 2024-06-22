f4dd480994ef86a4d887542f2c8d49c36742f4e2
Mingfei Li <ml@mingfei.io>
2024-06-21 17:57:35 -0700

    Refactor + simplify + InvertedPendulum exp-1

--- config.py
+++ config.py
@@ -1,14 +1,23 @@
-class Config():
-    n_batches = 5000
-    batch_size = 1
+class InvertedPendulumConfig():
+    n_episodes = 5_000
     policy_network_lr = 1e-4
     baseline_network_lr = 1e-3
     gamma = 1
-    n_batches_baseline = 0
+    use_baselines = False
     advantage_normalization = False
     record_freq = 1_000
-    # discrete = True
-    # game = 'CartPole-v0'
     discrete = False
     game = 'InvertedPendulum-v4'
-    exp_id = 'exp-44'
+    exp_id = 'exp-1'
+
+class CartPoleConfig():
+    n_episodes = 5_000
+    policy_network_lr = 1e-4
+    baseline_network_lr = 1e-3
+    gamma = 1
+    use_baselines = False
+    advantage_normalization = False
+    record_freq = 1_000
+    discrete = True
+    game = 'CartPole-v0'
+    exp_id = 'exp-1'
--- train.py
+++ train.py
@@ -1,9 +1,9 @@
-from config import Config
+from config import InvertedPendulumConfig, CartPoleConfig
 from gymnasium.experimental.wrappers import RecordVideoV0
-from logger import Logger
 from models import DiscretePolicyModel, ContinuousPolicyModel, BaselineModel
 from tqdm import tqdm
 from torch.distributions import Categorical, Normal
+from torch.utils.tensorboard import SummaryWriter
 
 import gymnasium as gym
 import numpy as np
@@ -21,37 +21,30 @@ def set_seed(seed):
     torch.backends.cudnn.benchmark = False
 
 class Agent():
-    def __init__(self, run_id):
-        self.config = Config()
+    def __init__(self, run_id, config):
         self.run_id = run_id
-        self.env = gym.make(self.config.game, render_mode='rgb_array')
-        # self.env = RecordVideoV0(
-        #     env=gym.make(self.config.game, render_mode='rgb_array'),
-        #     video_folder=f'results/{self.config.exp_id}/{run_id}/videos',
-        #     episode_trigger=lambda n: n%self.config.record_freq == 0,
-        # )
+        self.config = config
+        self.env = RecordVideoV0(
+            env=gym.make(config.game, render_mode='rgb_array'),
+            video_folder=f'results/{config.game}/{config.exp_id}/{run_id}/videos',
+            episode_trigger=lambda n: n%config.record_freq == 0,
+        )
         self.env.reset(seed=run_id)
         self.env.action_space.seed(run_id)
-        if self.config.discrete:
-            self.n_actions = self.env.action_space.n
-        else:
-            self.n_actions = self.env.action_space.shape[0]
 
-        self.logger = Logger(log_dir=f'results/{self.config.exp_id}/{run_id}/logs')
-
-        if self.config.discrete:
+        if config.discrete:
             self.policy_network = DiscretePolicyModel(
                 in_features=self.env.observation_space.shape[0],
-                out_features=self.n_actions,
+                out_features=self.env.action_space.n,
             )
         else:
             self.policy_network = ContinuousPolicyModel(
                 in_features=self.env.observation_space.shape[0],
-                out_features=self.n_actions,
+                out_features=self.env.action_space.shape[0],
             )
         self.policy_optimizer = torch.optim.Adam(
             self.policy_network.parameters(),
-            lr=self.config.policy_network_lr,
+            lr=config.policy_network_lr,
         )
 
         self.baseline_network = BaselineModel(
@@ -60,139 +53,97 @@ class Agent():
         )
         self.baseline_optimizer = torch.optim.Adam(
             self.baseline_network.parameters(),
-            lr=self.config.baseline_network_lr,
+            lr=config.baseline_network_lr,
         )
 
-    def sample_one_episode(self):
+        self.logger = SummaryWriter(
+            log_dir=f'results/{config.game}/{config.exp_id}/{run_id}/logs',
+        )
+
+    def sample_one_episode(self, step):
         states = []
-        actions = []
         rewards = []
+        log_probs = []
+        entropy = []
+
         state, _ = self.env.reset()
         while True:
-            self.policy_network.eval()
-            input = torch.tensor(state, dtype=torch.float32).unsqueeze(dim=0)
-            if self.config.discrete:
-                with torch.no_grad():
-                    prob = self.policy_network(input)
-                distribution = Categorical(prob.squeeze(dim=0))
-                action = distribution.sample().item()
-            else:
-                with torch.no_grad():
-                    mu, sigma = self.policy_network(input)
-                distribution = Normal(mu.squeeze(dim=0), sigma.squeeze(dim=0))
-                action = distribution.sample()
-
+            state = torch.tensor(state, dtype=torch.float32)
             states.append(state)
-            actions.append(action)
-            (
-                state, reward, terminated, truncated, _,
-            ) = self.env.step(action)
+
+            self.policy_network.train()
+            input = state.unsqueeze(dim=0)
+            if self.config.discrete:
+                probs = self.policy_network(input)[0]
+                distribution = Categorical(probs)
+            else:
+                mu, sigma = self.policy_network(input)
+                distribution = Normal(mu[0], sigma[0])
+            
+            action = distribution.sample()
+            log_probs.append(distribution.log_prob(action).sum())
+            entropy.append(distribution.entropy().sum())
+
+            if self.config.discrete:
+                action = action.item()
+            else:
+                action = action.numpy()
+
+            state, reward, terminated, truncated, _, = self.env.step(action)
             rewards.append(reward)
             if terminated or truncated:
                 break
 
-        r = torch.zeros(len(states))
-        for t in range(len(states) - 1, -1, -1):
-            r[t] = rewards[t]
-            if t < len(states) - 1:
-                r[t] += r[t+1] * self.config.gamma
-        s = torch.tensor(np.stack(states), dtype=torch.float32)
-        if self.config.discrete:
-            a = torch.tensor(actions)
-        else:
-            a = torch.stack(actions)
-        return s, a, r
+        rewards_to_go = rewards.copy()
+        for t in reversed(range(len(rewards_to_go) - 1)):
+            rewards_to_go[t] += self.config.gamma * rewards_to_go[t+1]
 
-    def sample(self):
-        states = []
-        actions = []
-        rewards = []
+        self.logger.add_scalar('episode_len', len(rewards), step)
+        self.logger.add_scalar('episode_reward', sum(rewards), step)
+        self.logger.add_scalar('entropy', torch.tensor(entropy).mean(), step)
 
-        for _ in range(self.config.batch_size):
-            s, a, r = self.sample_one_episode()
-            states.append(s)
-            actions.append(a)
-            rewards.append(r)
+        s = torch.stack(states)
+        rtg = torch.tensor(rewards_to_go)
+        lp = torch.stack(log_probs)
+        return s, rtg, lp
 
-        return torch.concat(states), torch.concat(actions), torch.concat(rewards)
+    def train_and_get_baseline(self, states, rewards_to_go, step):
+        self.baseline_network.train()
+        preds = self.baseline_network(states).squeeze(dim=1)
+        loss = nn.MSELoss()(preds, rewards_to_go)
+        self.baseline_optimizer.zero_grad()
+        loss.backward()
+        self.baseline_optimizer.step()
 
-    def train_baseline(self):
-        for _ in range(self.config.n_batches_baseline):
-            states, _, rewards = self.sample()
-            self.baseline_network.train()
-            baseline_preds = self.baseline_network(states).squeeze(dim=1)
-            baseline_loss = nn.MSELoss()(baseline_preds, rewards)
-            self.baseline_optimizer.zero_grad()
-            baseline_loss.backward()
-            self.baseline_optimizer.step()
-            self.logger.add_scalar('baseline_loss', baseline_loss.item())
-            self.logger.add_scalar('baseline_lr', self.config.baseline_network_lr)
-
-    def eval(self):
-        _, a, r = self.sample_one_episode()
-        self.logger.add_scalar('episode_len', len(r))
-        self.logger.add_scalar('episode_discounted_reward', r[0].item())
-        self.logger.add_scalar('action.avg', a.float().mean().item())
-        self.logger.add_scalar('action.max', a.float().max().item())
-        self.logger.add_scalar('action.min', a.float().min().item())
+        self.logger.add_scalar('baseline_loss', loss.item(), step)
+        self.logger.add_scalar('baseline_lr', self.config.baseline_network_lr, step)
+        return preds
 
     def train(self):
-        for i in tqdm(range(self.config.n_batches)):
-            states, actions, rewards = self.sample()
+        for i in tqdm(range(self.config.n_episodes)):
+            states, rewards_to_go, log_probs = self.sample_one_episode(i)
 
-            if self.config.n_batches_baseline > 0:
-                self.train_baseline()
-                self.baseline_network.eval()
-                with torch.no_grad():
-                    baselines = self.baseline_network(states).squeeze(dim=1)
-                self.logger.add_scalar('baseline', baselines.mean().item())
-            else:
-                baselines = 0
+            baselines = 0
+            if self.config.use_baselines:
+                baselines = self.train_and_get_baseline(states, rewards_to_go, i)
 
-            advantages = rewards - baselines
+            advantages = rewards_to_go - baselines
             if self.config.advantage_normalization:
                 advantages -= advantages.mean()
                 advantages /= advantages.std()
 
-            self.policy_network.train()
-            if self.config.discrete:
-                prob = self.policy_network(states)
-                distribution = Categorical(prob)
-                entropy = distribution.entropy()
-                log_prob = distribution.log_prob(actions)
-            else:
-                mu, sigma = self.policy_network(states)
-                distribution = Normal(mu, sigma)
-                entropy = distribution.entropy().sum(dim=1)
-                log_prob = distribution.log_prob(actions).sum(dim=1)
-
-                self.logger.add_scalar('mu.avg', mu.mean().item())
-                self.logger.add_scalar('mu.max', mu.max().item())
-                self.logger.add_scalar('mu.min', mu.min().item())
-                self.logger.add_scalar('sigma.avg', sigma.mean().item())
-                self.logger.add_scalar('sigma.max', sigma.max().item())
-                self.logger.add_scalar('sigma.min', sigma.min().item())
-
-            policy_loss = -torch.sum(advantages * distribution.log_prob(actions))
+            loss = -torch.sum(advantages * log_probs)
             self.policy_optimizer.zero_grad()
-            policy_loss.backward()
+            loss.backward()
             self.policy_optimizer.step()
 
-            self.eval()
-
-            self.logger.add_scalar('entropy.avg', entropy.mean().item())
-            self.logger.add_scalar('entropy.max', entropy.max().item())
-            self.logger.add_scalar('entropy.min', entropy.min().item())
-            self.logger.add_scalar('log_prob.avg', log_prob.mean().item())
-            self.logger.add_scalar('log_prob.max', log_prob.max().item())
-            self.logger.add_scalar('log_prob.min', log_prob.min().item())
-            self.logger.add_scalar('policy_loss', policy_loss.item())
-            self.logger.add_scalar('policy_lr', self.config.policy_network_lr)
-            self.logger.flush(i)
+            self.logger.add_scalar('policy_loss', loss.item(), i)
+            self.logger.add_scalar('policy_lr', self.config.policy_network_lr, i)
+            self.logger.flush()
     
         self.env.close()
 
 if __name__ == "__main__":
-    for run_id in [0]:#, 42, 1234, 9999, 11111]:
+    for run_id in [0, 42, 1234, 9999, 11111]:
         set_seed(run_id)
-        Agent(run_id).train()
+        Agent(run_id, InvertedPendulumConfig()).train()
